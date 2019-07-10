539f00ce46b683916f16007e64cdcdb79ca67906
Tunde Adepitan <tunde262@gmail.com>
2019-07-10 12:05:52 -0500

    product list grid

--- client/src/components/Overview/productOverview/ProductCard.js
+++ client/src/components/Overview/productOverview/ProductCard.js
@@ -37,7 +37,7 @@ class ProductCard extends Component {
         const { _id, title, img, price, inCart } = this.props.product;
 
         return (
-            <ProductWrapper className="mx-auto col-6 col-lg-3 my-3">
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
                     <div 
                         className="img-container p-5" 
